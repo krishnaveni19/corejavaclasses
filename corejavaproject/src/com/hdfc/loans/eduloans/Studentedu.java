4382bb6727947851d1b900f9acf388a3703fa53e
krishnaveni <krishnaveni19.rallabandi@gmail.com>
2019-03-28 22:58:58 -0700

    added second message in Studentedu class

--- corejavaproject/src/com/hdfc/loans/eduloans/Studentedu.java
+++ corejavaproject/src/com/hdfc/loans/eduloans/Studentedu.java
@@ -4,6 +4,7 @@ public class Studentedu {
 
 	public static void main(String[] args) {
 	System.out.println("Studentedu class is added in this package");
+	System.out.println("The above message is revorted");
 
 	}
 

0b5a5b0283a8d1c25ab50fb3cf8bcd51fcbd5460
krishna <krishna@ITDL007-LT-S.westernu.edu>
2019-03-28 22:43:36 -0700

    Revert "added message in Studentedu class"
    This reverts commit 67dd913d95be0a44e8261d4ab04955ea92786ad1.

--- corejavaproject/src/com/hdfc/loans/eduloans/Studentedu.java
+++ corejavaproject/src/com/hdfc/loans/eduloans/Studentedu.java
@@ -3,7 +3,7 @@ package com.hdfc.loans.eduloans;
 public class Studentedu {
 
 	public static void main(String[] args) {
-	System.out.println("Studentedu class is added in this package");
+		// TODO Auto-generated method stub
 
 	}
 
